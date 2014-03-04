8d7bc720d86ad7231e2b2cbef3f7abce36fdacec
Ilya Dmitrichenko <ilya@xively.com>
2014-03-04 19:24:05 +0000

    [coro] Add `YIELD_ON()` as it seems useful

--- src/libxively/xi_coroutine.h
+++ src/libxively/xi_coroutine.h
@@ -16,6 +16,23 @@ extern "C" {
 #define YIELD( state, ret )\
     state = __LINE__; return ret; case __LINE__:
 
+#define YIELD_ON( state, expression, ret )\
+{ \
+    if ( (expression) ) \
+    { \
+      state = __LINE__; return ret; case __LINE__:; \
+    } \
+};
+
+#define YIELD_UNTIL( state, expression, ret )\
+{ \
+    if ( (expression) ) \
+    { \
+      state = __LINE__; return ret; case __LINE__:; \
+      continue; \
+    } \
+};
+
 #define EXIT( state, ret )\
     state = 1; return ret;
 
