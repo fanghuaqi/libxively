75450b12be6b09b1912dbf93d7cdf1c34f9de2ac
Olgierd Humenczuk <olgierdh@gmail.com>
2014-03-20 00:53:10 +0100

    [test] added test for eventd handling of event dispatcher

--- src/tests/xi_dispatcher.h
+++ src/tests/xi_dispatcher.h
@@ -40,6 +40,21 @@ void continuation1( uint32_t* a )
     *a = 127;
 }
 
+void continuation1_1( uint32_t* a )
+{
+    *a += 1;
+}
+
+void continuation1_3( uint32_t* a )
+{
+    *a += 3;
+}
+
+void continuation1_5( uint32_t* a )
+{
+    *a += 5;
+}
+
 void continuation2( int a, void* b )
 {
     printf( "test %d, %p\n", a, b );
@@ -52,6 +67,7 @@ void continuation3( int a, void* b, char* c )
 
 static xi_evtd_instance_t* evtd_g_i = 0;
 static xi_evtd_handle_t evtd_handle_g;
+static xi_static_vector_t *fds_g_i = 0;
 
 void proc_loop( uint32_t* a )
 {
@@ -168,6 +184,86 @@ end:
     xi_evtd_destroy_instance( evtd_g_i );
 }
 
+void test_evtd_updates( void* data )
+{
+    (void) data;
+
+    uint32_t counter = 0;
+
+    evtd_g_i    = xi_evtd_create_instance();
+    fds_g_i     = xi_static_vector_create( 4 );
+
+    tt_assert( xi_evtd_register_fd( evtd_g_i, 15 ) != 0 );
+    tt_assert( xi_evtd_register_fd( evtd_g_i, 14 ) != 0 );
+    tt_assert( xi_evtd_register_fd( evtd_g_i, 12 ) != 0 );
+
+    {
+        xi_evtd_handle_t evtd_handle = { XI_EVTD_HANDLE_1_ID, .handlers.h1 = { &continuation1_1, &counter } };
+        xi_evtd_continue_when_evt( evtd_g_i, XI_EVENT_WANT_READ, evtd_handle, 15 );
+
+        xi_evtd_triplet_t* tmp = ( xi_evtd_triplet_t* ) evtd_g_i->handles_and_fd->array[ 0 ].value;
+
+        tt_assert( tmp->event_type                      == XI_EVENT_WANT_READ );
+        tt_assert( tmp->handle.handle_type              == XI_EVTD_HANDLE_1_ID );
+        tt_assert( tmp->handle.handlers.h1.a1           == &counter );
+        tt_assert( tmp->handle.handlers.h1.phandle_1    == &continuation1_1 );
+    }
+
+    {
+        xi_evtd_handle_t evtd_handle = { XI_EVTD_HANDLE_1_ID, .handlers.h1 = { &continuation1_3, &counter } };
+        xi_evtd_continue_when_evt( evtd_g_i, XI_EVENT_WANT_WRITE, evtd_handle, 14 );
+
+        xi_evtd_triplet_t* tmp = ( xi_evtd_triplet_t* ) evtd_g_i->handles_and_fd->array[ 1 ].value;
+
+        tt_assert( tmp->event_type                      == XI_EVENT_WANT_WRITE );
+        tt_assert( tmp->handle.handle_type              == XI_EVTD_HANDLE_1_ID );
+        tt_assert( tmp->handle.handlers.h1.a1           == &counter );
+        tt_assert( tmp->handle.handlers.h1.phandle_1    == &continuation1_3 );
+    }
+
+    {
+        xi_evtd_handle_t evtd_handle = { XI_EVTD_HANDLE_1_ID, .handlers.h1 = { &continuation1_5, &counter } };
+        xi_evtd_continue_when_evt( evtd_g_i, XI_EVENT_CLOSE, evtd_handle, 12 );
+
+        xi_evtd_triplet_t* tmp = ( xi_evtd_triplet_t* ) evtd_g_i->handles_and_fd->array[ 2 ].value;
+
+        tt_assert( tmp->event_type                      == XI_EVENT_CLOSE );
+        tt_assert( tmp->handle.handle_type              == XI_EVTD_HANDLE_1_ID );
+        tt_assert( tmp->handle.handlers.h1.a1           == &counter );
+        tt_assert( tmp->handle.handlers.h1.phandle_1    == &continuation1_5 );
+    }
+
+    xi_evtd_update_events( evtd_g_i, fds_g_i );
+
+    xi_static_vector_push( fds_g_i, ( void* ) ( intptr_t ) 12 );
+
+    tt_assert( counter == 0 );
+    xi_evtd_update_events( evtd_g_i, fds_g_i );
+    tt_assert( counter == 5 );
+
+    xi_static_vector_push( fds_g_i, ( void* ) ( intptr_t ) 15 );
+    counter = 0;
+
+    tt_assert( counter == 0 );
+    xi_evtd_update_events( evtd_g_i, fds_g_i );
+    tt_assert( counter == 6 );
+
+    xi_static_vector_push( fds_g_i, ( void* ) ( intptr_t ) 14 );
+    counter = 0;
+
+    tt_assert( counter == 0 );
+    xi_evtd_update_events( evtd_g_i, fds_g_i );
+    tt_assert( counter == 9 );
+
+    xi_evtd_unregister_fd( evtd_g_i, 12 );
+    xi_evtd_unregister_fd( evtd_g_i, 15 );
+    xi_evtd_unregister_fd( evtd_g_i, 14 );
+
+end:
+    xi_static_vector_destroy( fds_g_i );
+    xi_evtd_destroy_instance( evtd_g_i );
+}
+
 struct testcase_t dispatcher_tests[] = {
     /* Here's a really simple test: it has a name you can refer to it
        with, and a function to invoke it. */
@@ -175,6 +271,7 @@ struct testcase_t dispatcher_tests[] = {
     { "test_dispatcher_continuation1", test_continuation1, TT_ENABLED_, 0, 0 },
     { "test_dispatcher_processing_loop", test_handler_processing_loop, TT_ENABLED_, 0, 0 },
     { "test_register_fd", test_register_fd, TT_ENABLED_, 0, 0 },
+    { "test_evtd_updates", test_evtd_updates, TT_ENABLED_, 0, 0 },
     /* The array has to end with END_OF_TESTCASES. */
     END_OF_TESTCASES
 };
