7cae75c276262d3f1b2d4539b7332ab3c6005fe1
Wayne Ren <wei.ren@synopsys.com>
2015-04-16 10:47:58 +0800

    [build system] use / as dir separator

--- xively.mk
+++ xively.mk
@@ -34,7 +34,7 @@ MID_CSRCDIR += $(MID_XIVELY_C_API_CSRCDIR)
 MID_ASMSRCDIR += $(MID_XIVELY_C_API_ASMSRCDIR)
 
 # the dir to generate objs
-MID_XIVELY_C_API_OBJDIR = $(OUT_DIR)$(PS)middleware$(PS)xively
+MID_XIVELY_C_API_OBJDIR = $(OUT_DIR)/middleware/xively
 
 # find all the srcs in the target dirs
 MID_XIVELY_C_API_CSRCS = $(call get_csrcs, $(MID_XIVELY_C_API_CSRCDIR))
@@ -50,7 +50,7 @@ ifneq ($(MAKECMDGOALS),clean)
 endif
 
 # genearte library
-MID_LIB_XIVELY = $(OUT_DIR)$(PS)libmidxively.a
+MID_LIB_XIVELY = $(OUT_DIR)/libmidxively.a
 MID_LIBS += $(MID_LIB_XIVELY)
 
 $(MID_LIB_XIVELY): $(MID_XIVELY_C_API_OBJDIR) $(MID_XIVELY_C_API_COBJS) $(MID_XIVELY_C_API_ASMOBJS)
