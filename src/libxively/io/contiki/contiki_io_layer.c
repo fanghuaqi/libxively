d9ee96d3eb41dfa91a5c2f098b690d04a76bc7f1
Huaqi Fang <Huaqi.Fang@synopsys.com>
2015-06-02 17:20:29 +0800

    [COMMENT CHANGE] change /**  * Modified
    to
    /*
     * Modified
    
    to avoid include this comments into doxygen document

--- src/libxively/io/contiki/contiki_io_layer.c
+++ src/libxively/io/contiki/contiki_io_layer.c
@@ -1,6 +1,6 @@
 // Copyright (c) 2003-2014, LogMeIn, Inc. All rights reserved.
 // This is part of Xively C library, it is under the BSD 3-Clause license.
-/**
+/*
  * Modified for port to ARC processor
  * by Wayne Ren wei.ren@synopsys.com, Synopsys, Inc.
  */
--- src/libxively/xi_allocator_lwip.h
+++ src/libxively/xi_allocator_lwip.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2003-2014, LogMeIn, Inc. All rights reserved.
 // This is part of Xively C library, it is under the BSD 3-Clause license.
-/**
+/*
  * Modified for port to ARC processor
  * by Huaqi Fang huaqi.fang@synopsys.com, Synopsys, Inc.
  */
